256a67f3d1ff150d3f041ecba7e978e50924c170
durnberger <paul@durnberger.co.uk>
2024-05-02 11:33:56 +0100

    automated: 2024-05-02-1133

--- config/nvim/lua/settings/keymapping.lua
+++ config/nvim/lua/settings/keymapping.lua
@@ -1,6 +1,5 @@
 -- Keymapping
 --
-
 local function map(mode, lhs, rhs, opts)
     -- Set default values if not specified
     if opts.noremap == nil then
@@ -30,21 +29,21 @@ map("n", "<A-Right>", ":bnext<cr>", {})
 map("n", "<A-Left>", ":bprevious<cr>", {})
 
 -- ======================================================================================
--- Line Break using Ctrl-return
+-- Line break using Ctrl-return
 map("i", "<C-cr>", " <br /><cr>", {})
 
 -- ======================================================================================
 -- Telescope 
 local builtin = require('telescope.builtin')
-map('n', '<leader>ff', builtin.find_files, {})
-map('n', '<leader>fg', builtin.live_grep, {})
-map('n', '<leader>fb', builtin.buffers, {})
-map('n', '<leader>fh', builtin.help_tags, {})
+map('n', '<leader>ff', builtin.find_files, {})  -- find files in CWD
+map('n', '<leader>fg', builtin.live_grep, {})   -- searches for string in CWD
+map('n', '<leader>fb', builtin.buffers, {})     -- lists open buffers
+map('n', '<leader>fl', builtin.grep_string, {}) -- searches for string under cursor
+map('n', '<leader>fh', builtin.help_tags, {})   -- lists available help tags
 
 -- ======================================================================================
--- Tidy Tables
 -- Align tables in markdown 
--- (https://jcuenod.github.io/bibletech/2023/03/14/formatting-md-tables-in-neovim/)
+-- https://jcuenod.github.io/bibletech/2023/03/14/formatting-md-tables-in-neovim/
 map("n", "<leader>mt", "vip :!pandoc -t markdown-simple_tables<cr>", { desc = "Tidy table" })
 
 -- ======================================================================================
